32d8e03a358b476a089f22575f5e191725c418f5
Yusuf_D <ydemirci7@gmail.com>
2022-01-08 13:40:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+function playSound(letter){
+    switch (letter) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    }
+}
+
+function buildAnimation(key){
+    const letters = ["w", "a", "s", "d", "j", "k", "l"]
+    if (!letters.includes(key)) return;
+
+    let activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
+
+// listens for mouse clicks
+const buttons = document.querySelectorAll(".drum");
+
+buttons.forEach(button =>
+    button.addEventListener("click", function(){ 
+        playSound(this.innerHTML)
+        buildAnimation(this.innerHTML)
+    })
+)
+
+// listens for keyboard entries
+document.addEventListener("keydown", function(e){
+    playSound(e.key);
+    buildAnimation(e.key)
+})
